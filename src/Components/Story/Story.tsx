a19c01ff36008b20433e1d24b3108fc50e2b49fc
LLorintz <lorintz.levente@mechwart.com>
2025-04-05 14:29:56 +0200

    Add files via upload

---
+++ src/Components/Story/Story.tsx
@@ -0,0 +1,39 @@
+import './story.css'
+
+{/* 767px fölöt kerüljenek egymás mellé a story_data és story_images egymás mellé */}
+
+{/* Story_-title mérete legyen h1-font-size */}
+
+/* Section__title szövegét alakítsd át nagybeűssé a css-ben */
+
+const Story = () => {
+  return (
+    <section className="story">
+    <div className="story__container">
+        <div className="story__data">
+            <h2 className="section__title story__section-title">
+                Our Story
+            </h2>
+
+            <h1 className="story__title">
+                Inspirational Watch of <br/> this year
+            </h1>
+
+            <p className="story__description">
+                The latest and modern watches of this year, is available in various 
+                presentations in this store, discover them now.
+            </p>
+
+            <a href="#" className="button button--small">Discover</a>
+        </div>
+
+        <div className="story__images">
+            <img src="/img/story.png" alt="" className="story__img"/>
+            <div className="story__square"></div>
+        </div>
+    </div>
+</section>
+  )
+}
+
+export default Story
